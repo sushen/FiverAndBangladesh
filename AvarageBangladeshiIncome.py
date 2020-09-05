b4ec726e3233d9c9086e4b53f48604f61bdeaa90
Sushen Biswas <sushenbiswasaga@gmail.com>
2020-09-05 14:37:17 +0600

    Initial commit

---
+++ AvarageBangladeshiIncome.py
@@ -0,0 +1,25 @@
+loving_Bangladesh = 1360710350
+
+#total fiver sellar
+TotalSeller = 830000
+ActiveSeller = 255000
+
+#Bangladeshi Traffic 8%
+bangladesgiTraffic = 8
+
+
+বাংলাদেশি_সেলার = (ActiveSeller*8)/100
+
+
+print(বাংলাদেশি_সেলার)
+
+টোটাল_রোজগার = বাংলাদেশি_সেলার * 103
+
+print(টোটাল_রোজগার)
+
+total_remitance = 100000000
+
+fiver_remitance = total_remitance-টোটাল_রোজগার
+
+print(fiver_remitance)
+
